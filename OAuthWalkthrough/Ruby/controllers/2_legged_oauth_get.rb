410d4118deadc2cb586bbec540257d3db56e63a1
jenndox <doxtader@gmail.com>
2012-10-25 08:54:33 -0700

    Added a 2 legged OAuth example.

---
+++ OAuthWalkthrough/Ruby/controllers/2_legged_oauth_get.rb
@@ -0,0 +1,33 @@
+require 'rubygems'
+require 'oauth'
+require 'json'
+
+# These are the Fastpass credentials for a community
+
+CONSUMER_KEY    = "12345"
+CONSUMER_SECRET = "1234567890"
+
+# Set the http method and the API endpoint
+http_method     = :get
+api_host        = "http://api.getsatisfaction.com"
+
+consumer = OAuth::Consumer.new(CONSUMER_KEY, CONSUMER_SECRET, :site => api_host, :http_method => http_method)
+access_token = OAuth::AccessToken.new(consumer)
+
+
+request_headers = {'Content-Type' => 'application/json'}
+
+api_url_path    = "/companies/people/jenndox.json"
+
+# --- for get ---
+users_response = access_token.get(api_url_path, request_headers)
+users_hash = JSON.parse(users_response.body)
+
+users_hash['data'].each { |k|
+  user_id = k['id']
+  name = k['name']
+  email = k['shared_email']
+  
+
+  puts "user #{user_id} (#{name}) \t n. has email: #{email}"
+}
